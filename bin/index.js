34fa1224cca99672293f01c69257a306043ba318
Nikola Horvat <nikola.horvat@leaseplan.com>
2020-01-22 16:20:36 +0100

    feat: add list command

---
+++ bin/index.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+const vCLI = require("commander");
+const list = require("../lib/list/");
+
+/*******************************************/
+// list: list all imports
+// $ modim list <required single file or glob>
+// $ modim ls <required single file or glob>
+vCLI
+  .command("list <single file or glob>")
+  .alias("ls")
+  .action(list)
+  .description("List all imports.");
+/*******************************************/
+
+vCLI.parse(process.argv);
