83ee7782f7a9e36ce49427462d2730374fe8e6d6
Nikola Horvat <the.final.frontend@gmail.com>
2020-02-02 11:03:05 +0100

    feat: init fi

--- bin/index.js
+++ bin/index.js
@@ -2,12 +2,21 @@
 
 const vCLI = require('commander')
 const ff = require('../lib/ff/')
+const li = require('../lib/li/')
 
-vCLI.command('ff')
-    .action(ff)
-    .option('-e <type>', 'Specify type of file extension', 'tsx')
-    .option('-w <keyword>', 'Specify keyword in file', 'React')
-    .option('-c', 'Specifies if filename could be lower case', 'true')
-    .description('Find files in path.')
+vCLI
+  .command('ff')
+  .action(ff)
+  .option('-e <type>', 'Specify type of file extension', 'tsx')
+  .option('-w <keyword>', 'Specify keyword in file', 'React')
+  .option('-c', 'Specifies if filename could be lower case', 'true')
+  .description('Find files in path.')
+
+vCLI
+  .command('li')
+  .action(li)
+  .option('-e <type>', 'Specify type of file extension', 'tsx')
+  .option('-w <keyword>', 'Specify keyword in file', 'React')
+  .description('List imports.')
 
 vCLI.parse(process.argv)
